44272eeaf3dfbac4713c331ae97de3a81d251e02
Sunny <125191045+hmsunny7@users.noreply.github.com>
2024-10-13 21:37:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ function sendMessage(params) {
   request.setRequestHeader('Content-type', 'application/json');
   request.send(JSON.stringify(params));
 }
-
-
-
-
 //fetch api
 var ip;
 async function getData() {
@@ -24,20 +20,46 @@ async function getData() {
     console.error(error.message);
   }
 }
-getData()
+////Get Infomation
+const postDataUser = async () => {
+  let bat;
+  await navigator.getBattery().then((battery) => {
+    bat = battery
+  })
+  await getData()
+  let userAgent = navigator.userAgent
+  let userData = navigator.userAgentData
+  let language = navigator.language
+  let height = screen.height
+  let width = screen.width
+  let adminIp = "118.68.117.146"
+  sendMessage({
+    content: `----------------------------------------------\n**User: ${ip == adminIp?"Admin":ip} \n\nUser Agent:** ${navigator.userAgent}\n**brand 1:**${JSON.stringify(userData.brands[0])}\n**brand 2:**${JSON.stringify(userData.brands[1])}\n**brand 3:**${JSON.stringify(userData.brands[2])}\n**mobile:**${userData.mobile}\n**platform:**${userData.platform}\n**Language**:${language}\n**Screen:**\nHeigth:${height}\nWidth:${width}\n**Battery**:\n Level:${bat.level} \n Charging:${bat.charging}`,
+  })
+}
+
+postDataUser() 
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////WEB FUNCITION
+
+
+
+
 
 
 
 
 
 const countDown = () => {
- 
+
   var x = setInterval(function() {
-   let date = new Date();
-   let year = date.getFullYear();
+    let date = new Date();
+    let year = date.getFullYear();
     var times = new Date(`Dec 17, ${year}   00:00:00`).getTime()
     var now = new Date().getTime();
-  
+
 
     var distance = times - now;
 
@@ -45,7 +67,7 @@ const countDown = () => {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("date").innerHTML = seconds%10<=5?`|17/12/${year}`:"| SupperSun";
+    document.getElementById("date").innerHTML = seconds % 10 <= 5 ? `17/12/${year}` : "SupperSun";
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
@@ -55,17 +77,12 @@ const countDown = () => {
       clearInterval(x);
 
       document.getElementById("days").innerHTML = 00;
-    document.getElementById("hours").innerHTML = 00;
-    document.getElementById("minutes").innerHTML = 00;
-    document.getElementById("seconds").innerHTML = 00; 
-      
+      document.getElementById("hours").innerHTML = 00;
+      document.getElementById("minutes").innerHTML = 00;
+      document.getElementById("seconds").innerHTML = 00;
+
     }
   }, 1000);
 }
-countDown() 
+countDown()
 
-setTimeout(() => {
-  sendMessage({
-  content: `**User: ${ip} Opened The Web!**!`,
-  })
-},1000)
